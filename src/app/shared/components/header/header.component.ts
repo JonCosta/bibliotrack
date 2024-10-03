13ede0114c82f404710ad305caada827177a695c
JonCosta <jonjcosta@gmail.com>
2024-10-03 10:15:33 -0300

    Feat: Icons in main menu
    - Added icons to header's menu items

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
@@ -11,12 +12,14 @@ import { RouterModule } from '@angular/router';
         MatToolbarModule,
         MatMenuModule,
         MatButtonModule,
-        RouterModule],
+        RouterModule,
+        MatIconModule
+    ],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
-    title: string = "BiblioTrack"
+    title: string = "BiblioTrack";
 
 }
