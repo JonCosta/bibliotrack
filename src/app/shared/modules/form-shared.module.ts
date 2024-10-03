5f18d93955ae4db8c2bb7403f90d1312021f04c2
JonCosta <jonjcosta@gmail.com>
2024-10-03 11:31:33 -0300

    Feat: Layout for Form and List
    - Changed general layout for User Form and List

--- src/app/shared/modules/form-shared.module.ts
+++ src/app/shared/modules/form-shared.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     imports: [
+        MatButtonModule,
+        MatCardModule,
         MatFormFieldModule,
         MatInputModule,
-        ReactiveFormsModule,
         MatSelectModule,
-        MatButtonModule
+        ReactiveFormsModule,
     ],
     exports: [
+        MatButtonModule,
+        MatCardModule,
         MatFormFieldModule,
         MatInputModule,
-        ReactiveFormsModule,
         MatSelectModule,
-        MatButtonModule
+        ReactiveFormsModule,
     ]
 })
 export class FormSharedModule { }
--- src/app/shared/modules/list-shared.module.ts
+++ src/app/shared/modules/list-shared.module.ts
@@ -1,6 +1,7 @@
 import { DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
 import { ListItemActionsComponent } from '../components/list-item-actions/list-item-actions.component';
@@ -8,18 +9,20 @@ import { ListItemActionsComponent } from '../components/list-item-actions/list-i
 @NgModule({
     declarations: [],
     imports: [
-        MatTableModule,
         DatePipe,
+        ListItemActionsComponent,
         MatButtonModule,
-        RouterModule,
-        ListItemActionsComponent
+        MatCardModule,
+        MatTableModule,
+        RouterModule
     ],
     exports: [
-        MatTableModule,
         DatePipe,
-        RouterModule,
-        ListItemActionsComponent
-
+        ListItemActionsComponent,
+        MatButtonModule,
+        MatCardModule,
+        MatTableModule,
+        RouterModule
     ]
 })
 export class ListSharedModule { }
