cad78f5b45a823a9808f894e726b5ebb9fdeb31b
JonCosta <jonjcosta@gmail.com>
2024-10-11 10:10:12 -0300

    Feat: Author and Publisher Forms
    Forms for Author and Publisher set up following the standard style rules

--- src/app/pages/authors/author-form/author-form.component.ts
+++ src/app/pages/authors/author-form/author-form.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormSharedModule } from '../../../shared/modules/form-shared.module';
 
 @Component({
-  selector: 'app-author-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './author-form.component.html',
-  styleUrl: './author-form.component.scss'
+    selector: 'app-author-form',
+    standalone: true,
+    imports: [
+        FormSharedModule
+    ],
+    templateUrl: './author-form.component.html',
+    styleUrl: './author-form.component.scss'
 })
 export class AuthorFormComponent {
+
+    authorForm: FormGroup;
+
+    constructor(
+        private formBuilder: FormBuilder
+    ) {
+        this.authorForm = this.configureForm();
+    }
+
+    onSubmitForm() {
+        let author = this.authorForm.getRawValue();
+        console.log(author);
+    }
+
+    private configureForm() {
+        return this.formBuilder.group({
+            id: [null],
+            name: [null, Validators.required]
+        });
+    }
 
 }
--- src/app/pages/publishers/publisher-form/publisher-form.component.ts
+++ src/app/pages/publishers/publisher-form/publisher-form.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormSharedModule } from '../../../shared/modules/form-shared.module';
 
 @Component({
-  selector: 'app-publisher-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './publisher-form.component.html',
-  styleUrl: './publisher-form.component.scss'
+    selector: 'app-publisher-form',
+    standalone: true,
+    imports: [
+        FormSharedModule
+    ],
+    templateUrl: './publisher-form.component.html',
+    styleUrl: './publisher-form.component.scss'
 })
 export class PublisherFormComponent {
+
+    publisherForm: FormGroup;
+
+    constructor(
+        private formBuilder: FormBuilder
+    ) {
+        this.publisherForm = this.configureForm();
+    }
+
+    onSubmitForm() {
+        let publisher = this.publisherForm.getRawValue();
+        console.log(publisher);
+    }
+
+    private configureForm() {
+        return this.formBuilder.group({
+            id: [null],
+            name: [null, Validators.required]
+        });
+    }
 
 }
