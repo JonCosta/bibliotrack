b19ebe01c2d9ec919b7634d0181406cef3d0cbea
JonCosta <jonjcosta@gmail.com>
2024-10-11 09:33:26 -0300

    Feat: User Form
    Created general style rules to User Form, allowing easy use in other forms too

--- src/app/shared/modules/form-shared.module.ts
+++ src/app/shared/modules/form-shared.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -5,9 +6,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { EnumPipe } from '../pipes/enum.pipe';
 
 @NgModule({
     imports: [
+        CommonModule,
+        EnumPipe,
         MatButtonModule,
         MatCardModule,
         MatFormFieldModule,
@@ -16,6 +20,8 @@ import { MatSelectModule } from '@angular/material/select';
         ReactiveFormsModule,
     ],
     exports: [
+        CommonModule,
+        EnumPipe,
         MatButtonModule,
         MatCardModule,
         MatFormFieldModule,
