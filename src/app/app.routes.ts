0d098026d20d0d82b3d89fd613f400ce25dbade5
JonCosta <jonjcosta@gmail.com>
2024-10-14 14:52:07 -0300

    Feat: Page Not Found
    Added new component for Page Not Found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { BookFormComponent } from './pages/books/book-form/book-form.component';
 import { BooksComponent } from './pages/books/books.component';
 import { HomeComponent } from './pages/home/home.component';
 import { InventoryComponent } from './pages/inventory/inventory.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { PublisherFormComponent } from './pages/publishers/publisher-form/publisher-form.component';
 import { PublishersComponent } from './pages/publishers/publishers.component';
 import { UserFormComponent } from './pages/users/user-form/user-form.component';
@@ -12,23 +13,43 @@ import { UsersComponent } from './pages/users/users.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'home',
         component: HomeComponent
     },
     {
         path: 'books',
         children: [
-            { path: '', component: BooksComponent },
-            { path: 'new', component: BookFormComponent },
-            { path: 'edit/:id', component: BookFormComponent }
+            {
+                path:'',
+                component: BooksComponent
+            },
+            {
+                path: 'new', component: BookFormComponent,
+                data: {
+                    breadcrumb: 'New Book'
+                }
+            },
+            {
+                path: 'edit/:id', component: BookFormComponent,
+                data: {
+                    breadcrumb: 'Edit Book'
+                }
+            }
         ]
     },
     {
         path: 'authors',
         children: [
-            { path: '', component: AuthorsComponent },
-            { path: 'new', component: AuthorFormComponent },
-            { path: 'edit/:id', component: AuthorFormComponent }
+            {
+                path: '',
+                component: AuthorsComponent
+            },
+            {
+                path: 'new', component: AuthorFormComponent
+            },
+            {
+                path: 'edit/:id', component: AuthorFormComponent
+            }
         ]
     },
     {
@@ -52,5 +73,11 @@ export const routes: Routes = [
         children: [
             { path: '', component: InventoryComponent }
         ]
+    },
+    {
+        path: '', redirectTo: 'home', pathMatch: 'full'
+    },
+    {
+        path: '**', component: PageNotFoundComponent
     }
 ];
